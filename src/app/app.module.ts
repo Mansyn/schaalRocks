92ef500946acfa0ea008aec6b057617b075f3f46
Mansyn <stephenschaal@penguin>
2019-11-17 17:01:43 -0500

    remove routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AngularFireAuthModule } from '@angular/fire/auth'
@@ -10,19 +9,16 @@ import { AngularFirestoreModule } from '@angular/fire/firestore'
 import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material/'
 
 import { environment } from '../environments/environment';
-import { HomeComponent } from './routes/home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
