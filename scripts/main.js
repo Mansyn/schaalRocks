4606dfa25fe5a7e945ce0ac0a88d978c7c45cd2e
Mansyn <stephenschaal@penguin>
2019-11-28 19:11:06 -0500

    dragging

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,38 @@
 // Shortcuts to DOM Elements.
 var rocks = document.querySelectorAll('.rocks image');
 
-function handleDragStart(e) {
-    this.style.opacity = '0.4';  // this / e.target is the source node.
+function onDragStart(event) {
+    event
+        .dataTransfer
+        .setData('text/plain', event.target.id);
+
+    event
+        .currentTarget
+        .style
+        .opacity = '0.4';
+}
+
+function onDragOver(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    const id = event
+      .dataTransfer
+      .getData('text');
+  
+    const draggableElement = document.getElementById(id);
+    const dropzone = event.target;
+    
+    dropzone.appendChild(draggableElement);
+  
+    event
+      .dataTransfer
+      .clearData();
 }
 
 // Bindings on load.
 window.addEventListener('load', function () {
 
-    [].forEach.call(rocks, function (rock) {
-        rock.addEventListener('dragstart', handleDragStart, false);
-    });
 
 }, false);
