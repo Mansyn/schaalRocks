4c14c39426bfc9b985f0543f156062fe71a60d89
Stephen Schaal <stephen.schaal@gmail.com>
2019-12-12 15:35:06 -0500

    fixing

--- scripts/schaal.ts
+++ scripts/schaal.ts
@@ -18,41 +18,56 @@ export class Schaal {
         this.createRock('blood-drop')
     }
 
-    private allowDrop = (event) => {
-        event.preventDefault();
-        event.currentTarget.style.background = '#7f8082';
+    private dragenter = (event) => {
+        if (event.target.className == "dropzone") {
+            event.currentTarget.style.background = 'rgba(24, 23, 23, 0.9)'
+        }
+    }
+
+    private dragleave = (event) => {
+        if (event.target.className == "dropzone") {
+            event.currentTarget.style.background = 'rgba(24, 23, 23, 0.65)'
+        }
     }
 
     private drop = (event) => {
         event.preventDefault();
-        const data = event.dataTransfer.getData("text/plain");
-        const element = document.querySelector(`#${data}`);
-        event.currentTarget.style.background = 'white'
+        const data = event.dataTransfer.getData('text/plain')
+        const element = document.querySelector(`#${data}`)
+        event.currentTarget.style.background = 'rgba(24, 23, 23, 0.65)'
         try {
-            event.target.appendChild(element);
+            event.target.appendChild(element)
         } catch (error) {
             console.warn("you can't move the item to the same place")
         }
     }
 
     private dragStart = (event) => {
-        event.dataTransfer.setData("text/plain", event.target.id)
+        event.target.style.opacity = '0.4'
+        event.dataTransfer.setData('text/plain', event.target.id)
+    }
+
+    private dragEnd = (event) => {
+        event.target.style.opacity = '1'
     }
 
     private createDrop() {
-        var element = document.createElement('div')
-        element.setAttribute('class', 'dropzone')
-        element.ondragover = this.allowDrop
-        element.ondrop = this.drop
-        document.getElementById('board').appendChild(element)
+        var zone = document.createElement('div')
+        zone.setAttribute('class', 'dropzone')
+        zone.ondragenter = this.dragenter
+        zone.ondragleave = this.dragleave
+        zone.ondrop = this.drop
+        document.getElementById('board').appendChild(zone)
     }
 
     private createRock(name: string) {
-        var element = document.createElement('img')
-        element.setAttribute('src', '/assets/' + name + '.svg')
-        element.setAttribute('id', name)
-        element.setAttribute('draggable', 'true')
-        element.ondragstart = this.dragStart
-        document.getElementById('rocks').appendChild(element)
+        var rock = document.createElement('img')
+        rock.setAttribute('src', '/assets/' + name + '.svg')
+        rock.setAttribute('id', name)
+        rock.setAttribute('draggable', 'true')
+        rock.setAttribute('class', 'rock')
+        rock.ondragstart = this.dragStart
+        rock.ondragend = this.dragEnd
+        document.getElementById('rocks').appendChild(rock)
     }
 }
