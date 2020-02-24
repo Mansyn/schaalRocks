4c35a9b7922487ff6a8ec622a0fdacca089b3803
Stephen Schaal <stephen.schaal@gmail.com>
2020-02-24 14:34:30 -0500

    styling update

--- scripts/app.ts
+++ scripts/app.ts
@@ -23,3 +23,13 @@ let rock = new Rock()
 window.addEventListener('DOMContentLoaded', function (event) {
     rock.init()
 })
+
+window.onorientationchange = function () {
+    var orientation = window.orientation;
+    switch (orientation) {
+        case 0:
+        case 90:
+        case -90: window.location.reload()
+            break;
+    }
+}
